cd22abad0cf01471c45a63f53a38493f4911cd01
Mike Heijmans <parabuzzle@gmail.com>
2014-08-30 08:39:32 -0700

    set env for development

--- _serve.sh
+++ _serve.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export JEKYLL_ENV='development'
+export JEKYLL_SKIP_DISQUS=true
+export JEKYLL_SKIP_SOCIAL=false
+
 jekyll server &
-jekyll build --watch
+jekyll build --watch --drafts
