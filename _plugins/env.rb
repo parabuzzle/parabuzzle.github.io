7c6456183ce5c9830447b7019de1641b90b88bcd
Mike Heijmans <parabuzzle@gmail.com>
2014-08-30 08:42:21 -0700

    remove development/production variable

--- _plugins/env.rb
+++ _plugins/env.rb
@@ -3,7 +3,6 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
  
     def generate(site)
-      site.config['env'] = ENV['JEKYLL_ENV'] || 'production'
       site.config['skip_disqus'] = ENV['JEKYLL_SKIP_DISQUS'] || false
       site.config['skip_social'] = ENV['JEKYLL_SKIP_SOCIAL'] || false
     end
